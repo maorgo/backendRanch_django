1676e7a88b90bd82ef91dac5c9b2b0761f0eff61
Maor <goaz.maor@gmail.com>
2018-07-05 22:28:16 +0300

    support for multiple tags

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=150)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=130)
     introduction = models.TextField(max_length=300)
     content = models.TextField(max_length=99999)
     slug = models.SlugField(max_length=150, default='')
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToOneRel(Tag)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
