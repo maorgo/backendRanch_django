e520343b0dd0d87515790060f904d48294f393c5
Maor <goaz.maor@gmail.com>
2018-07-09 09:58:51 +0300

    Port to taggit

--- backendRanch/views.py
+++ backendRanch/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
-
+from taggit.models import Tag
 from posts.models import Post
+
+TAGS = Tag.objects.all()
 
 
 def return_index(request):
     # Get 5 latest posts ordered by date
     all_posts = Post.objects.all().order_by('-created_on')[:5]
-    context = {'main_content': all_posts}
+    context = {'main_content': all_posts,
+               'tags': TAGS}
     return render(request, 'posts.html', context=context)
 
 
 def about(request):
-    return render(request, 'about.html')
+    context = {'tags': TAGS}
+    return render(request, 'about.html', context=context)
 
 
 def return_posts_by_tag(request, tag):
     # Get all posts belonging to a specific tag
     # todo: instead of query, query it once at server startup and store it in cache
     p = Post.objects.filter(tags__name__in=[tag]).order_by('-created_on')
-    context = {'main_content': p}
+    context = {'main_content': p,
+               'tags': TAGS}
     return render(request, 'posts.html', context=context)
