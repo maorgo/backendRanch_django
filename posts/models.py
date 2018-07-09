76f65df4efff91b221fffc638cb0e99f91ea4919
Maor <goaz.maor@gmail.com>
2018-07-09 11:22:43 +0300

    Port to taggit

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.utils.safestring import mark_safe
 from django.template.defaultfilters import slugify
 from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    introduction = mark_safe(models.TextField(max_length=500))
-    content = mark_safe(models.TextField(max_length=99999))
+    introduction = models.TextField(max_length=500)
+    content = models.TextField(max_length=99999)
     slug = models.SlugField(max_length=150, default='')
     tags = TaggableManager()
     created_on = models.DateTimeField(auto_now_add=True)
