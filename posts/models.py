c56e23c9dc56cdda6ea051c6c222d8d0e3cb01a0
Maor <goaz.maor@gmail.com>
2018-07-09 22:11:55 +0300

    Add wysiwyg support

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from taggit.managers import TaggableManager
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
-    introduction = models.TextField(max_length=500)
-    content = models.TextField(max_length=99999)
+    introduction = RichTextField()
+    content = RichTextField()
     slug = models.SlugField(max_length=150, default='')
     tags = TaggableManager()
     created_on = models.DateTimeField(auto_now_add=True)
